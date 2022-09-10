a44f3a1381328217a82e9fbd3ef70957715c173f
HP <HP@DESKTOP-FC7K6VA>
2022-09-10 15:03:57 +0530

    Good

---
+++ filters/src/com/text/servlet.java
@@ -0,0 +1,31 @@
+package com.text;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/servlet")
+public class servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		HttpSession session=request.getSession(true);
+		session.invalidate();
+		out.println("successfully logoout");
+
+
+
+	}
+
+}
