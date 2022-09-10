86819eca8d9a7b7c811428722235e398dfc11e5a
HP <HP@DESKTOP-FC7K6VA>
2022-09-10 15:12:59 +0530

    bye

---
+++ springcore/src/main/java/springcore/customer.java
@@ -0,0 +1,31 @@
+package springcore;
+
+public class customer
+{
+private int id;
+private String name;
+private String city;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public void display()
+{
+	System.out.println("id:"+id);
+System.out.println("name:"+name);
+System.out.println("city:"+city);
+}}
